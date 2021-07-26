fa3fabe8a7518755a16d92ad49392f05c57ce3c5
wcrd <11898748+wcrd@users.noreply.github.com>
2021-07-26 21:52:18 +1000

    Remove unused files

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
 import brick_xlsx_validator as bv
 import os
 
-df, bad_refs = bv.validate(os.path.join(r"C:\Users\WillDavidson\Desktop\DB\equipment2.xlsx"))
+df, bad_refs = bv.validate(os.path.join(r"C:\Users\test\Desktop\DB\equipment2.xlsx"))
