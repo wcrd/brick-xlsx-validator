9449f4d02e6696ac8e5aa8b3e279be94423e83c7
wcrd <11898748+wcrd@users.noreply.github.com>
2021-08-06 21:24:05 +1000

    Update example

--- example.py
+++ example.py
@@ -3,7 +3,8 @@ import os
 
 # model_path = r"C:\Users\WillDavidson\Desktop\DB-BrickModelInput-V6.xlsx"
 # model_path = r"C:\Users\WillDavidson\OneDrive - Switch Automation\R&D\Brick Modelling\sites\Carrier CIB\20210728 - Carrier-CiB_subParted _newFormat.xlsx"
-model_path = r"C:\Users\WillDavidson\OneDrive - Switch Automation\R&D\Brick Modelling\sites\Carrier CIB\20210728 - Carrier-CiB _newFormat.xlsx"
+# model_path = r"C:\Users\WillDavidson\OneDrive - Switch Automation\R&D\Brick Modelling\sites\Carrier CIB\20210728 - Carrier-CiB _newFormat.xlsx"
 # model_path = r"C:\Users\WillDavidson\Downloads\eau_clair_equip.xlsx"
+model_path = r"C:\Users\WillDavidson\OneDrive - Switch Automation\R&D\Brick Modelling\sites\Carrier Lerner\20210808 - Carrier-Lerner _newFormat.xlsx"
 
 df, bad_refs, bad_classes = bv.validate(os.path.join(model_path))
