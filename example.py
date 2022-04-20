ebc15bfd9b31512ed160fc3d2fc15f33c5538fe7
wcrd <11898748+wcrd@users.noreply.github.com>
2022-04-20 11:40:39 -0600

    Add paramter to set which field is referenced by the relationships for validation purposes

--- example.py
+++ example.py
@@ -7,4 +7,5 @@ import os
 # model_path = r"C:\Users\WillDavidson\Downloads\eau_clair_equip.xlsx"
 model_path = r"C:\Users\WillDavidson\OneDrive - Switch Automation\R&D\Brick Modelling\sites\Carrier Lerner\20210808 - Carrier-Lerner _newFormat.xlsx"
 
-df, bad_refs, bad_classes = bv.validate(os.path.join(model_path))
+df, bad_refs, bad_classes = bv.validate(os.path.join(model_path), reference_field=("Brick", "label"))
+
